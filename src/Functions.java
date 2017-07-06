8372992a662b1ea1b4295e0b66e5765ffa3e9f94
maths-nerd <maths_nerd@yahoo.com>
2017-07-06 18:14:56 +0400

    Added split current ip by octet in integer. working on getclass function

--- src/Functions.java
+++ src/Functions.java
@@ -9,10 +9,42 @@ public class Functions{
 	try {
 
 		ip = InetAddress.getLocalHost();
-		System.out.println("Current IP address : " + ip.getHostAddress());
+		String thisip = ip.getHostAddress();
+		String[] tempoctet = {"","","",""};
+		System.out.println("Current IP address : " + thisip);
+		int cur = 0;
+		for(int i=0; i<thisip.length();i++){
+			if(thisip.charAt(i) != '.'){
+				tempoctet[cur] += thisip.charAt(i);
+			}
+			else{
+				cur++;
+			}
+		}
+		cur = 0;
+		
+		int[] octet = new int[4];
+		for(int i = 0; i<tempoctet.length; i++){
+			octet[i] = Integer.parseInt(tempoctet[i]);
+		}
+		
+		printarray(octet);
+		
+		
 	} catch (UnknownHostException e) {
 		e.printStackTrace();
 	}
+   }// end main
+   
+   public static void printarray(int[] arr){
+	   for(int i = 0; i < arr.length; i++){
+		   System.out.println(arr[i]);
+	   }
+   }
+   
+   public static char getClass(int[] arr){
+	   int firstoctet = arr[0];
+	   //--------------neeed to write this function------------
    }
 
 }
