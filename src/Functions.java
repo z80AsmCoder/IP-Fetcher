bb634bd241e189bdbf6a117b8f896d78cf042162
maths-nerd <maths_nerd@yahoo.com>
2017-07-06 17:29:23 +0400

    Trying Inet for the first time

---
+++ src/Functions.java
@@ -0,0 +1,18 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Functions{
+
+   public static void main(String[] args){
+
+	InetAddress ip;
+	try {
+
+		ip = InetAddress.getLocalHost();
+		System.out.println("Current IP address : " + ip.getHostAddress());
+	} catch (UnknownHostException e) {
+		e.printStackTrace();
+	}
+   }
+
+}
